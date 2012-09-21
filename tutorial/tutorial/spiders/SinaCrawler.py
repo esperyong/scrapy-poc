aeea207d6793b61c2a079a3eaebca1ca9fc5b0e8
刘 勇 <liuyong@192.168.0.150>
2012-09-21 18:39:37 +0800

    研究爬虫

---
+++ tutorial/tutorial/spiders/SinaCrawler.py
@@ -0,0 +1,22 @@
+from scrapy.selector import HtmlXPathSelector
+from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
+from scrapy.contrib.spiders import CrawlSpider, Rule
+from tutorial.items import DmozItem
+
+class SinacrawlerSpider(CrawlSpider):
+    name = 'SinaCrawler'
+    allowed_domains = ['www.sina.com.cn']
+    start_urls = ['http://news.sina.com.cn/']
+
+    rules = (
+        Rule(SgmlLinkExtractor(allow=r'Items/'), callback='parse_item', follow=True),
+    )
+
+    def parse_item(self, response):
+        hxs = HtmlXPathSelector(response)
+        print response.body
+        i = DmozItem()
+        #i['domain_id'] = hxs.select('//input[@id="sid"]/@value').extract()
+        #i['name'] = hxs.select('//div[@id="name"]').extract()
+        #i['description'] = hxs.select('//div[@id="description"]').extract()
+        return i
--- tutorial/tutorial/spiders/dmoz_spider.py
+++ tutorial/tutorial/spiders/dmoz_spider.py
@@ -1,19 +1,34 @@
 # coding: utf-8
 from scrapy.spider import BaseSpider
-
+from scrapy.utils.misc import arg_to_iter
+from scrapy.http.request import Request
+from scrapy.selector import HtmlXPathSelector
 class DmozSpider(BaseSpider):
     name = "dmoz"
-    allowed_domains = ["dmoz.org"]
-    start_urls = [
-        "http://www.dmoz.org/Computers/Programming/Languages/Python/Books/",
-        "http://www.dmoz.org/Computers/Programming/Languages/Python/Resources/",
-        "http://news.sina.com.cn/china/",
-        "http://zhusuban.xdf.cn/?utm_campaign=jituan_sohu_shouye_wenzilian_zhusuban_717z731&utm_medium=sohu&utm_source=cpm&utm_content=&utm_term=",
-    ]
+    allowed_domains = ["sina.com.cn"]
+
+    def __init__(self, name=None, **kwargs):
+        if name is not None:
+            self.name = name
+        elif not getattr(self, 'name', None):
+            raise ValueError("%s must have a name" % type(self).__name__)
+        self.__dict__.update(kwargs)
+        if not hasattr(self, 'start_urls'):
+            self.start_urls = [
+                "http://news.sina.com.cn",
+                "http://www.sina.com.cn",
+            ]
 
     def parse(self, response):
         print "爬爬<%s>" % response.url
-        filename = response.url.split("/")[-2]
-        open(filename, 'wb').write(response.body)
+        print response.headers
+        hxs = HtmlXPathSelector(response)
+        print hxs.select('//title').extract()
+        if response.headers.has_key('Last-Modified'):
+            response_headers = {'If-Modified-Since':
+                                response.headers['Last-Modified']}
+        req = Request(response.url, dont_filter=True,headers=response_headers)
+        reqs = [req]
+        return reqs
 
 
